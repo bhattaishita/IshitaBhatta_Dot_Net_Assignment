0b17f3162768949c1902a1392b3631ce819910c7
acer <bhatta.ishita@gmail.com>
2025-02-14 13:03:51 +0545

    Add project files.

---
+++ RestautantManagementSystem1/Areas/Identity/Data/RestautantManagementSystem1User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace RestautantManagementSystem1.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the RestautantManagementSystem1User class
+public class RestautantManagementSystem1User : IdentityUser
+{
+}
+
