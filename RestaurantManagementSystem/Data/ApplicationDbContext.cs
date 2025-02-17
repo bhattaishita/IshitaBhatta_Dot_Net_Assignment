b7afe74c2c4160fc01d531a17d941cbb4df1129d
acer <bhatta.ishita@gmail.com>
2025-02-17 11:19:35 +0545

    Add project files.

---
+++ RestaurantManagementSystem/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RestaurantManagementSystem.Models;
+
+namespace RestaurantManagementSystem.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<RestaurantManagementSystem.Models.Menu> Menus { get; set; } = default!;
+    }
+}
---
+++ RestaurantManagementSystem/Models/Menu.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantManagementSystem.Models
+{
+    public class Menu
+    {
+        [Key] // Marks foodId as the Primary Key
+        public int FoodId { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Food name cannot exceed 100 characters.")]
+        public string FoodName { get; set; }
+
+        [Required]
+        [Range(0.01, 10000, ErrorMessage = "Price must be between 0.01 and 10,000.")]
+        public decimal Pricing { get; set; }
+
+        [Range(1, 5, ErrorMessage = "Review rating must be between 1 and 5.")]
+        public double Review { get; set; }
+    }
+}
